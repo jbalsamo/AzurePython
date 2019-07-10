0aa195b06d5d3649f46dc2f35a9bab3bef9459f1
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2019-07-10 11:26:27 -0400

    numbers work

--- joeCherryPy/__init__.py
+++ joeCherryPy/__init__.py
@@ -6,18 +6,20 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    num = req.params.get('number')
-    exp = req.params.get('exponent')
-    value = null
+    num = int(req.params.get('number'))
+    exp = int(req.params.get('exponent'))
+    value = 0
     if not (num and exp):
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            num = req_body.get('number')
-            exp = req_body.get('exponent')
+            num = int(req_body.get('number'))
+            exp = int(req_body.get('exponent'))
             value = num**exp;
+    else:
+        value = num**exp
 
     if value:
         return func.HttpResponse(f"Answer: {value}")
